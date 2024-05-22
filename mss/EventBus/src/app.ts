da4b6d2810344827b5ee30e0940b110852103978
su315123 <103009301+su315123@users.noreply.github.com>
2024-05-22 20:36:52 -0300

    feat(EventBus): add EventBus

---
+++ mss/EventBus/src/app.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import axios from 'axios'
+const app = express()
+app.use(express.json())
+
+const { PORT } = process.env
+app.post('/event', (req, res) => {
+    const evento = req.body
+    axios.post('http://localhost:1000/event', evento)
+    res.end()
+})
+
+app.listen(PORT, () => console.log(`Barramento. Porta: ${PORT}.`))
