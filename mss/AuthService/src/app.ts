bee6341901c435dd798ff6120dabe9ab1a2afb07
edu510 <www.eduardo.f.n@gmail.com>
2024-05-28 02:29:51 -0300

    feat(EventBus): Add lost events tracking and microservices discovery

--- mss/AuthService/src/app.ts
+++ mss/AuthService/src/app.ts
@@ -7,6 +7,7 @@ const app = express()
 const {Pool} = require('pg')
 
 const { PORT, USER, PASSWORD } = process.env
+const SERVICE_NAME = "AuthService"
 
 const pool = new Pool({
     user: USER,
@@ -79,7 +80,7 @@ app.post('/validate', async function(req, res){
 
         if (isValid) {
             let userValidatedEvent: UserValidatedEvent = { auth_token: auth_token, validation_timestamp: new Date().toISOString() }
-            axios.post('http://localhost:10000/event', { payload: userValidatedEvent, eventType: "userValidatedSuccessfulEvent" })
+            axios.post('http://localhost:10000/event', { service_name: SERVICE_NAME, payload: userValidatedEvent, event_type: "userValidatedSuccessfulEvent" })
                 .catch(() => console.log("erro no barramento"))
 
             let validationSuccessfulResponse: ValidationSuccessfulResponse = { auth_token: auth_token }
@@ -108,20 +109,21 @@ let events: Record<string, (arg:any)=>Promise<any>> = {
 
 interface Event{
     payload:string
-    eventType:string
+    event_type:string
 }
 app.post('/event',  async (req, res) => {
-    let {payload,eventType}:Event = req.body;
-    let event = events[eventType];
+    let {payload,event_type}:Event = req.body;
+    let event = events[event_type];
     if(event){
         try {
             await event(payload);
         }
         catch (e) {
-            console.log(`error treating event: ${eventType}, message: ${e}`)
-            res.status(500).json({ error: `error treating event: ${eventType}, message: ${e}`})
+            console.log(`error treating event: ${event_type}, message: ${e}`)
+            res.status(500).json({ error: `error treating event: ${event_type}, message: ${e}`})
         }
     }
+    res.end()
 
 });
 
--- mss/EventBus/src/app.ts
+++ mss/EventBus/src/app.ts
@@ -4,13 +4,86 @@ import express from 'express'
 import axios from 'axios'
 const app = express()
 app.use(express.json())
+const {Pool} = require('pg')
 
-const { PORT } = process.env
-app.post('/event', (req, res) => {
-    const evento = req.body
-    axios.post('http://localhost:1000/event', evento)
-    axios.post('http://localhost:2000/event', evento)
+const { PORT, USER, PASSWORD } = process.env
+
+const pool = new Pool({
+    user: USER,
+    host: 'localhost',
+    database: 'EventBusService',
+    password: PASSWORD,
+    port: 5432,
+})
+
+/*  *** CRIAÇÃO DA TABELA
+
+DROP TABLE IF EXISTS Services CASCADE;
+CREATE TABLE Services (
+	service_name VARCHAR(20) PRIMARY KEY,
+	host VARCHAR(30),
+	port INTEGER
+);
+
+DROP TABLE IF EXISTS LostEvents CASCADE;
+CREATE TABLE LostEvents (
+	lost_event_id SERIAL PRIMARY KEY,
+	target_service VARCHAR(20),
+	emmiter_service VARCHAR(20),
+	event_type VARCHAR(20),
+	payload JSON,
+	FOREIGN KEY (target_service) REFERENCES Services (service_name),
+	FOREIGN KEY (emmiter_service) REFERENCES Services (service_name)
+	ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+*/
+
+interface Service {
+    service_name: string,
+    host: string,
+    port: number
+}
+
+interface Event {
+    service_name: string
+    payload: Object,
+    event_type: string
+}
+
+const AddServiceToDatabase = async (service_name: string, host: string, port: number) => {
+    await pool.query('INSERT INTO Services (service_name, host, port) VALUES ($1, $2, $3) ON CONFLICT (service_name) DO UPDATE SET host = EXCLUDED.host, port = EXCLUDED.port', [service_name, host, port])
+}
+
+const AddLostServiceToDatabase = async (target_service: string, emmiter_service: string, event_type: string, payload: Object) => {
+    await pool.query('INSERT INTO LostEvents (target_service, emmiter_service, event_type, payload) VALUES ($1, $2, $3, $4)', [target_service, emmiter_service, event_type, payload])
+}
+
+const getServices = async function(): Promise<Array<Service>> {
+    return (await pool.query('SELECT * FROM Services')).rows
+}
+
+app.post('/event', async (req, res) => {
+    console.log("Barramento recebeu event...")
+    const event: Event = req.body
+    const services: Array<Service> = await getServices()
+
+    services.forEach(s => {
+        axios.post(`http://${s.host}:${s.port}/event`, event).catch(
+            () => {AddLostServiceToDatabase(s.service_name, event.service_name, event.event_type, event.payload)}
+        )
+    });
     res.end()
+})
+
+app.post('/service-discovery', async (req, res) => {
+    const {service_name, host, port}: Service = req.body
+    await AddServiceToDatabase(service_name, host, port)
+    res.end()
+})
+
+app.post('/lost-events-recovery', async (req, res) => {
+
 })
 
 app.listen(PORT, () => console.log(`Barramento. Porta: ${PORT}.`))
