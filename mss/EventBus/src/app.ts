2ab1c7eb37f19b66abfcc4f8605d4faa9c1d57c8
su315123 <103009301+su315123@users.noreply.github.com>
2024-05-29 00:57:24 -0300

    feat(EventBus): add lost event recovery endpoint

--- mss/EventBus/src/app.ts
+++ mss/EventBus/src/app.ts
@@ -29,34 +29,47 @@ DROP TABLE IF EXISTS LostEvents CASCADE;
 CREATE TABLE LostEvents (
 	lost_event_id SERIAL PRIMARY KEY,
 	target_service VARCHAR(20),
-	emmiter_service VARCHAR(20),
+	emitter_service VARCHAR(20),
 	event_type VARCHAR(20),
 	payload JSON,
 	FOREIGN KEY (target_service) REFERENCES Services (service_name),
-	FOREIGN KEY (emmiter_service) REFERENCES Services (service_name)
+	FOREIGN KEY (emitter_service) REFERENCES Services (service_name)
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 */
 
 interface Service {
-    service_name: string,
-    host: string,
+    service_name: string
+    host: string
     port: number
 }
 
 interface Event {
     service_name: string
-    payload: Object,
+    payload: Object
     event_type: string
+}
+
+interface Lost_Event {
+    lost_event_id: number
+    target_service: string
+    emitter_service: string
+    event_type: string
+    payload: Object
+
 }
 
 const AddServiceToDatabase = async (service_name: string, host: string, port: number) => {
     await pool.query('INSERT INTO Services (service_name, host, port) VALUES ($1, $2, $3) ON CONFLICT (service_name) DO UPDATE SET host = EXCLUDED.host, port = EXCLUDED.port', [service_name, host, port])
 }
 
-const AddLostServiceToDatabase = async (target_service: string, emmiter_service: string, event_type: string, payload: Object) => {
-    await pool.query('INSERT INTO LostEvents (target_service, emmiter_service, event_type, payload) VALUES ($1, $2, $3, $4)', [target_service, emmiter_service, event_type, payload])
+const AddLostServiceToDatabase = async (target_service: string, emitter_service: string, event_type: string, payload: Object) => {
+    await pool.query('INSERT INTO LostEvents (target_service, emitter_service, event_type, payload) VALUES ($1, $2, $3, $4)', [target_service, emitter_service, event_type, payload])
+}
+
+const RemoveLostServiceFromDatabase = async(lost_event_id: number) => {
+    await pool.query('DELETE FROM LostEvents WHERE lost_event_id = $1', [lost_event_id])
 }
 
 const getServices = async function(): Promise<Array<Service>> {
@@ -83,7 +96,20 @@ app.post('/service-discovery', async (req, res) => {
 })
 
 app.post('/lost-events-recovery', async (req, res) => {
-
+    const {service_name} = req.body
+    const lostEvents: Array<Lost_Event> = (await pool.query('SELECT * FROM LostEvents WHERE target_service = $1', [service_name])).rows
+    const {host, port} = (await pool.query('SELECT host,port FROM Services WHERE service_name = $1', [service_name])).rows[0]
+    lostEvents.forEach(e => {
+        const event: Event = {service_name: e.emitter_service, payload : e.payload, event_type : e.event_type}
+        axios.post(`http://${host}:${port}/event`, event).then(
+            () => {
+                RemoveLostServiceFromDatabase(e.lost_event_id)
+            }
+        ).catch(e => console.log(e))
+    })
+    res.end()
 })
+
+
 
 app.listen(PORT, () => console.log(`Barramento. Porta: ${PORT}.`))
