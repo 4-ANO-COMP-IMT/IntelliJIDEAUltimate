2e47fee2134fa9bb4ad32b6df4853cae29b70a2b
Felipe(Comunalti) <frps2003@gmail.com>
2024-07-16 17:19:07 -0300

    testing reverted

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.tsx",
     output: {
-        path: path.resolve(dirname, "dist"),
+        path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
     resolve: {
@@ -14,16 +13,16 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /.tsx?$/,
+                test: /\.tsx?$/,
                 use: "ts-loader",
                 exclude: /node_modules/
             },
             {
-                test: /.(sass|css)$/,
+                test: /\.(sass|css)$/,
                 use: ["style-loader", "css-loader"]
             },
             {
-                test: /.(png|jpe?g|gif|svg|webp)$/i,
+                test: /\.(png|jpe?g|gif|svg|webp)$/i,
                 use: [
                     {
                         loader: "file-loader",
@@ -38,19 +37,13 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./public/index.html"
-        }),
-        new ESLintPlugin({
-            extensions: ["ts", "tsx", "js"],
-            context: path.resolve(dirname, "src"),
-            cache: true,
-            emitWarning: true,
-            failOnError: false
         })
     ],
     devServer: {
         static: {
             directory: path.join(__dirname, "public")
         },
+
         compress: true,
         port: 3002
     }
