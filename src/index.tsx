d3bce9560983d419fa732ac1eb0a9628e18d8e9c
Felipe(Comunalti) <frps2003@gmail.com>
2024-07-25 18:41:45 -0300

    build(config): update configuration files and entry point

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const container = document.getElementById("root");
