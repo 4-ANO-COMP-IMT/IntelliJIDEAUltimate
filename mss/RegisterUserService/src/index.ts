cf72dfd97917b22a09b7538a9117d4a772f1f90b
Felipe(Comunalti) <frps2003@gmail.com>
2024-09-01 21:31:19 -0300

    refactor(RegisterUserService): reorganiza queries de banco de dados e manipulador de erros

--- mss/RegisterUserService/src/index.ts
+++ mss/RegisterUserService/src/index.ts
@@ -1,60 +1,101 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './routes';
+// import routes from './routes';
 import { PORT } from './config/env';
-import { errorHandler } from './middlewares/errorHandler';
-import { sleep } from './utils/sleep';
-import axios from 'axios';
-import { publishToExchange } from './services/rabbitMQService';
-import { events } from './services/eventService';
-
+// import { errorHandler } from './middlewares/errorHandler';
+import {Consumer,connectToRabbitMQ,startConsuming, } from '@intelij-ultimate/rabbitmq-utility';
+import { LoginConsumer } from '@intelij-ultimate/session-utility';
+import {ResgistrationPublisherSingleton} from '@intelij-ultimate/user-utility';
+import { User, NewUser, getUserByUsername, createUser} from '@intelij-ultimate/user-utility';
+import { authMiddleware, adminMiddleware } from '@intelij-ultimate/middleware-utility';
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/api', routes);
-app.use(errorHandler);
+app.use(adminMiddleware);
+app.post('/register', async (req, res) => {
+    const { new_username, new_password } = req.body;
+    console.log('Registering user...');
+    console.log(`Username: ${new_username}`);
+    console.log(`Password: ${new_password}`);
 
-const myService = {
-  service_name: "RegisterUserService",
-  host: "localhost",
-  port: PORT,
-};
+    const newUser: NewUser = new NewUser(new_username, new_password);
 
-const startup = async (attempts: number) => {
-  while (attempts > 0) {
-    try {
-      await axios.post("http://localhost:10000/service-discovery", myService);
-      console.log("EventBus acknowledged this service");
-      break;
-    } catch (e) {
-      console.log(`${attempts} attempts for discovery`);
-      await sleep(800);
-      attempts--;
+    let user = await getUserByUsername(newUser.getUsername())
+    if (user) {
+        return res.status(400).json({ message: 'Username already exists' });
     }
-  }
+    user = await createUser(newUser.getUsername(), newUser.getPassword(), newUser.getIsAdmin());
+    console.log(`User registered: ${user.username} with id: ${user.user_id}`);
 
-  if (attempts <= 0) {
-    console.log("No response from EventBus. Ending service...");
-  }
-};
-
-startup(10);
-
-app.post('/event', async (req, res) => {
-  const { payload, event_type } = req.body;
-  const event = events[event_type];
-  if (event) {
-    try {
-      await event(payload);
-      res.status(200).end();
-    } catch (e) {
-      console.log(`Error handling event: ${event_type}, message: ${e}`);
-      res.status(500).json({ error: `Error handling event: ${event_type}, message: ${e}` });
-    }
-  } else {
-    res.status(400).json({ message: 'Unknown event type' });
-  }
+    await ResgistrationPublisherSingleton.getInstance().publish(user);
+    
+    res.status(200).json({ message: 'User registered' });
 });
+// app.use('/api', routes);
+// app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`RegisterUser service running on port ${PORT}`));
+const startUp = async() => {
+    console.log('wait 10 seconds before starting up ...');
+    await new Promise(resolve => setTimeout(resolve, 10000));
+    await new Promise(resolve => setTimeout(resolve, 4000));
+    console.log('starting up');
+
+    let connection = await connectToRabbitMQ();
+    await ResgistrationPublisherSingleton.getInstance().start();
+
+
+    let consumers: Consumer[] = [
+        LoginConsumer.getInstance()
+    ];
+    startConsuming(connection, consumers);
+
+    app.listen(PORT, () => console.log(`RegisterUser service running on port ${PORT}`));
+}
+
+
+startUp();
+
+
+// const myService = {
+//   service_name: "RegisterUserService",
+//   host: "localhost",
+//   port: PORT,
+// };
+
+// const startup = async (attempts: number) => {
+//   while (attempts > 0) {
+//     try {
+//       await axios.post("http://localhost:10000/service-discovery", myService);
+//       console.log("EventBus acknowledged this service");
+//       break;
+//     } catch (e) {
+//       console.log(`${attempts} attempts for discovery`);
+//       await sleep(800);
+//       attempts--;
+//     }
+//   }
+
+//   if (attempts <= 0) {
+//     console.log("No response from EventBus. Ending service...");
+//   }
+// };
+
+// startup(10);
+
+// app.post('/event', async (req, res) => {
+//   const { payload, event_type } = req.body;
+//   const event = events[event_type];
+//   if (event) {
+//     try {
+//       await event(payload);
+//       res.status(200).end();
+//     } catch (e) {
+//       console.log(`Error handling event: ${event_type}, message: ${e}`);
+//       res.status(500).json({ error: `Error handling event: ${event_type}, message: ${e}` });
+//     }
+//   } else {
+//     res.status(400).json({ message: 'Unknown event type' });
+//   }
+// });
+
--- mss/RegisterUserService/src/middlewares/errorHandler.ts
+++ mss/RegisterUserService/src/middlewares/errorHandler.ts
@@ -1,6 +1,6 @@
-import { Request, Response, NextFunction } from 'express';
+// import { Request, Response, NextFunction } from 'express';
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
-};
+// export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+//   console.error(err.stack);
+//   res.status(500).json({ message: 'Something went wrong!' });
+// };
