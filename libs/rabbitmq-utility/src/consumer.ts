530c4ccb8a6844587a3cedc43df4e8f509205977
Felipe(Comunalti) <frps2003@gmail.com>
2024-09-07 22:10:01 -0300

    refactor(rabbitmq-utility): reorganizar consumidores, remover consumer.ts

--- libs/rabbitmq-utility/src/consumer.ts
+++
@@ -1,8 +0,0 @@
-
-import amqp from 'amqplib';
-
-export interface Consumer{
-    queue: string;
-    exchange: string;
-    consumeFunction: (msg: amqp.ConsumeMessage | null, channel: amqp.Channel) => Promise<void>;
-};
