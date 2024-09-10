596591032994c98fa0d4ce4dd95955a7423ae941
edu510 <www.eduardo.f.n@gmail.com>
2024-09-10 03:22:16 -0300

    refactor(front): Melhor implementacao do ReactRouter, usando createBrowserRouter

--- apps/frontend-temp/src/components/App.tsx
+++ apps/frontend-temp/src/components/App.tsx
@@ -1,21 +1,47 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Classificate from './pages/Classificate';
-import Header from "./Header/Header";
+import SignInPage from './pages/SignInPage';
+import ClassificationPage from './pages/ClassificationPage';
+import HomePage from './pages/HomePage';
+import AdminRegisterPage from "./pages/AdminRegisterPage";
 
+import Header from "./Header/Header";
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import WelcomePage from "./pages/WelcomePage";
+import PageLayout from "./layouts/PageLayout";
+
+
+// Rotas principais e aninhadas com tipagem
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,  // Rota principal
+  },
+  {
+    path: '/sign-in',
+    element: <SignInPage />, // Página de login
+  },
+  {
+	path: '/classification',
+    element: <ClassificationPage />, // Página de login
+  },
+  {
+	path: '/admin-register',
+	element: <AdminRegisterPage />
+  },
+  {
+    path: '/welcome',
+    element: <WelcomePage />
+    },
+    
+]);
+
+
+
+// Função principal do app com tipagem
 const App: React.FC = () => {
-  return (
-	<>
-		<Header />
-		<Router>
-		<Routes>
-			<Route path="/" element={<Login />} />
-			<Route path="/classificate" element={<Classificate />} />
-		</Routes>
-		</Router>
-	</>
-  );
+	return <RouterProvider router={router} />;
 };
+
 
 export default App;
