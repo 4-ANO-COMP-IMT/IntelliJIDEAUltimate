b404846d955e00f6b5c08b691da61fa31a2e3bc7
edu510 <www.eduardo.f.n@gmail.com>
2024-09-13 18:44:09 -0300

    refactor(image-utility): Atualiza interfaces e metodos para novo formato da tabela images

--- libs/image-utility/src/interfaces.ts
+++ libs/image-utility/src/interfaces.ts
@@ -1,7 +1,25 @@
-export type Image = {
+export type ImageDB = {
   image_id:number;
   filename:string;
   filetype:string;
-  image_token:string;
 }
 
+export type Image = {
+  filename:string;
+  filetype:string;
+}
+
+export type ImageAllocation = {
+  image_id: number;
+  image_url: string;
+}
+
+/* 
+
+CREATE TABLE images(
+    image_id SERIAL PRIMARY KEY,
+    filename VARCHAR(255),
+    filetype VARCHAR(255)
+)
+
+*/
